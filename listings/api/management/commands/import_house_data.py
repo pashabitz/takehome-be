1eb40693702e134ea8ecccdac2d0a94efe6e920a
Pasha Bitz <pavelbitz@gmail.com>
2025-05-12 17:00:00 -0700

    Basic house model and import

--- listings/api/management/commands/import_house_data.py
+++ listings/api/management/commands/import_house_data.py
@@ -1,12 +1,40 @@
+import os
 from django.core.management.base import BaseCommand, CommandError
+from api.models import House
 
 class Command(BaseCommand):
     help = 'Imports data about houses'
+    def __init__(self):
+        super().__init__()
+        self.data_path = 'sample-data'
 
     def add_arguments(self, parser):
         # TODO: Add any arguments here
         pass
 
+    def process_one_csv_file(self, filename):
+        lines = []
+        with open(os.path.join(self.data_path, filename), 'r') as file:
+            header_line = file.readline()
+            for line in file:
+                fields = line.strip().split(',')
+                # add to lines
+                lines.append(fields)
+        return lines
+
+    def load_data(self):
+        records_written = 0
+        for filename in os.listdir(self.data_path):
+            data = self.process_one_csv_file(filename)
+            for line in data:
+                # print(line)
+                try:
+                    house = House.from_csv_line(line)
+                    house.save()
+                    records_written += 1
+                except Exception as e:
+                    raise CommandError(f"Error processing line: {line}, Error: {e}")
+        print(f"Successfully imported {records_written} records.")
+
     def handle(self, *args, **options):
-        # TODO: implement your import command
-        self.stdout.write("Import to be implemented.")
+        self.load_data()
--- listings/api/models.py
+++ listings/api/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from datetime import datetime
 
-# TODO: Create your models here.
+class House(models.Model):
+    area_unit = models.CharField(max_length=50)
+    bedrooms = models.FloatField(null=True)
+    bathrooms = models.FloatField(null=True)
+    home_size = models.FloatField(null=True)
+    home_type = models.CharField(max_length=50)
+    last_sold = models.DateField(null=True)
+    last_sold_price = models.FloatField(null=True)
+    link = models.URLField()
+    price = models.FloatField()
+    property_size = models.FloatField(null=True)
+    rent_price = models.FloatField(null=True)
+    rent_zestimate = models.FloatField(null=True)
+    rent_zestimate_last_updated = models.DateField(null=True)
+    tax_value = models.FloatField()
+    tax_year = models.IntegerField()
+    year_built = models.IntegerField(null=True)
+    zestimate = models.FloatField(null=True)
+    zestimate_last_updated = models.DateField(null=True)
+    zillow_id = models.CharField(max_length=50)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.address} - ${self.price}"
+
+    def string_price_to_float(self, price_str):
+        """
+        Convert a string price to a float.
+        This function removes any commas and dollar signs from the string.
+        Supports formats:
+        $715K
+        $1.93M
+        """
+        price_str = price_str.replace('$', '').replace(',', '')
+        if 'K' in price_str:
+            return float(price_str.replace('K', '')) * 1000
+        elif 'M' in price_str:
+            return float(price_str.replace('M', '')) * 1000000
+        else:
+            return float(price_str)
+
+    @staticmethod
+    def from_csv_line(line):
+        date_format = '%m/%d/%Y'
+        house = House()
+        house.area_unit = line[0]
+        house.bathrooms = None if line[1] == '' else float(line[1])
+        house.bedrooms = None if line[2] == '' else float(line[2])
+        house.home_size = None if line[3] == '' else float(line[3])
+        house.home_type = line[4]
+        house.last_sold = None if line[5] == '' else datetime.strptime(line[5], date_format).date()
+        house.last_sold_price = None if line[6] == '' else float(line[6])
+        house.link = line[7]
+        house.price = House.string_price_to_float(house, line[8])
+        house.property_size = None if line[9] == '' else float(line[9])
+        house.rent_price = None if line[10] == '' else float(line[10])
+        house.rent_zestimate = None if line[11] == '' else float(line[11])
+        house.rent_zestimate_last_updated = None if line[12] == '' else datetime.strptime(line[12], date_format).date()
+        house.tax_value = float(line[13])
+        house.tax_year = int(line[14])
+        house.year_built = None if line[15] == '' else int(line[15])
+        house.zestimate = None if line[16] == '' else float(line[16])
+        house.zestimate_last_updated = None if line[17] == '' else datetime.strptime(line[17], date_format).date()
+        house.zillow_id = line[18]
+        house.address = line[19]
+        house.city = line[20]
+        house.state = line[21]
+        house.zip_code = line[22]
+        return house
